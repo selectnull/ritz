f23ad4bfd13e533d672b058097abcee237ce7cbc
Sasha Matijasic <sasha@selectnull.com>
2022-04-22 16:56:54 +0200

    Remove secret_key

--- project/settings.py
+++ project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=oteg+vy9##n398lahb1y6@fbysz3j)v*vwc^@f@o%w84swok9'
+SECRET_KEY = os.environ['APP_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
