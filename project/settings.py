e8474c1ea7ac862aeb9348feacbf18fbf97b856c
Sasha Matijasic <sasha@selectnull.com>
2022-04-26 08:32:09 +0200

    Change database path

--- project/settings.py
+++ project/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': HOME_DIR / 'db' / 'db.sqlite3',
+        'NAME': HOME_DIR / 'db' / f'{PROJECT_NAME}.sqlite3',
     }
 }
 
