8f78bda15bd75c9aea1aa37026e65c5bf6267492
Sasha Matijasic <sasha@selectnull.com>
2022-04-26 16:52:05 +0200

    CORS, allow all domains

--- project/settings.py
+++ project/settings.py
@@ -126,3 +126,6 @@ STATIC_ROOT = HOME_DIR / 'public_files' / PROJECT_NAME / 'static'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+## CORS
+CORS_ALLOW_ALL_ORIGINS = True
